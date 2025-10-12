67fdf3ab59ef7bc35eb35a298fde0999cc27c432
xamsemi <xamsemi@googlemail.com>
2025-10-12 08:37:00 +0200

    test mit nginx durchgeführt. funtkion ist identisch.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const session = require('express-session');
+const argon2 = require('argon2');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
-// JSON body parser
 app.use(express.json());
 
 // Endpoint, der Nachrichten empfängt
-app.post('/message', (req, res) => {
+app.post('/api/message', (req, res) => {
     const { text } = req.body;  // <-- hier lesen wir den Body aus
     console.log('Nachricht erhalten:', text);
     let reply;
@@ -19,7 +26,14 @@ app.post('/message', (req, res) => {
     }
     res.json({ status: 'success', reply });
 });
+// Endpoint, der Nachrichten empfängt
+app.post('/nginx/restart', (req, res) => {
+    console.log('NginX');
 
+});
+
+
+// Server starten
 app.listen(port, () => {
     console.log(`Backend läuft auf http://localhost:${port}`);
 });
